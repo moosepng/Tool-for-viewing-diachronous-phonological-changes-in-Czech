9263803b5d03585513169df4158f45724b3d1d32
moosepng <88934611+moosepng@users.noreply.github.com>
2023-12-04 00:24:50 +0100

    rl

---
+++ slabicne-rl.py
@@ -0,0 +1,15 @@
+import re
+
+def slabicne_rl(text):
+    patterns = [
+        (r"([tkdgcčďsšzžnlrř])r", r"\1r̥"),
+        (r"([tkdgcčďsšzžnlrř])l", r"\1l̥")
+    ]
+
+    for pattern, replacement in patterns:
+        text = re.sub(pattern, replacement, text)
+
+    return text
+
+result = slabicne_rl("bratr nesl krve")
+print(result)
