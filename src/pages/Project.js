cd658ab6e01dc03b2843cccf07dea5d803017c67
moose.png <88934611+moosepng@users.noreply.github.com>
2024-06-27 17:41:31 +0200

    Update Project.js
    -

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -6,11 +6,8 @@ function Project() {
     <div className="project-container">
       <h2>O projektu</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent dapibus. Maecenas fermentum, sem in pharetra
-        pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Neque porro quisquam est, qui dolorem ipsum
-        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
-        magnam aliquam quaerat voluptatem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
-        egestas. Et harum quidem rerum facilis est et expedita distinctio.
+        Tento projekt vznikl v rámci 
+        <a href="https://is.muni.cz/th/c5heu/sasinek-bc-final.pdf">bakalářské práce</a> Tomáše Sasínka, studenta Počítačové lingvistiky na Masarykově univerzitě.
       </p>
     </div>
   );
